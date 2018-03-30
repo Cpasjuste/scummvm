22d462f219e7171ebb8ed17b731fe2c278c18185
Cpasjuste <cpasjuste@gmail.com>
2018-03-30 08:25:08 +0200

    switch: minor changes

--- backends/platform/sdl/switch/switch.mk
+++ backends/platform/sdl/switch/switch.mk
@@ -1,5 +1,6 @@
 scummvm.nro: $(EXECUTABLE)
 	elf2nro $(EXECUTABLE) scummvm.nro
+	mkdir -p /home/cpasjuste/.config/RyuFs/sdmc/switch/scummvm/data
 	cp $(srcdir)/backends/vkeybd/packs/vkeybd_default.zip /home/cpasjuste/.config/RyuFs/sdmc/switch/scummvm/data
 	cp $(srcdir)/backends/vkeybd/packs/vkeybd_small.zip /home/cpasjuste/.config/RyuFs/sdmc/switch/scummvm/data
 	cp $(DIST_FILES_THEMES) /home/cpasjuste/.config/RyuFs/sdmc/switch/scummvm/data
