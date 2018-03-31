51791d0bf4eaf9499ad5db6151f87f6c41c09be3
cpasjuste <cpasjuste@gmail.com>
2018-03-31 16:19:32 +0200

    switch: add packaging

--- backends/platform/sdl/switch/switch.mk
+++ backends/platform/sdl/switch/switch.mk
@@ -1,12 +1,17 @@
 scummvm.nro: $(EXECUTABLE)
-	elf2nro $(EXECUTABLE) scummvm.nro
-	mkdir -p /home/cpasjuste/.config/RyuFs/sdmc/switch/scummvm/data
-	cp $(srcdir)/backends/vkeybd/packs/vkeybd_default.zip /home/cpasjuste/.config/RyuFs/sdmc/switch/scummvm/data
-	cp $(srcdir)/backends/vkeybd/packs/vkeybd_small.zip /home/cpasjuste/.config/RyuFs/sdmc/switch/scummvm/data
-	cp $(DIST_FILES_THEMES) /home/cpasjuste/.config/RyuFs/sdmc/switch/scummvm/data
+	mkdir -p $(srcdir)/switch_release/scummvm/data
+	mkdir -p $(srcdir)/switch_release/scummvm/doc
+	nacptool --create "ScummVM" "Cpasjuste" "1.0" $(srcdir)/switch_release/scummvm.nacp
+	elf2nro $(EXECUTABLE) $(srcdir)/switch_release/scummvm/scummvm.nro --icon=$(srcdir)/dists/switch/icon.jpg --nacp=$(srcdir)/switch_release/scummvm.nacp
+	rm -f $(srcdir)/switch_release/scummvm.nacp
+	cp $(srcdir)/backends/vkeybd/packs/vkeybd_default.zip $(srcdir)/switch_release/scummvm/data
+	cp $(srcdir)/backends/vkeybd/packs/vkeybd_small.zip $(srcdir)/switch_release/scummvm/data
+	cp $(DIST_FILES_THEMES) $(srcdir)/switch_release/scummvm/data
 ifdef DIST_FILES_ENGINEDATA
-	cp $(DIST_FILES_ENGINEDATA) /home/cpasjuste/.config/RyuFs/sdmc/switch/scummvm/data
+	cp $(DIST_FILES_ENGINEDATA) $(srcdir)/switch_release/scummvm/data
 endif
+	cp $(DIST_FILES_DOCS) $(srcdir)/switch_release/scummvm/doc/
+	cd $(srcdir)/switch_release && zip -r ../scummvm_switch.zip . && cd ..
 
 .PHONY: scummvm.nro
 
